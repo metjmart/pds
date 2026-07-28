deb8a7a2b722b44abe22a6052a9b48d6a73b1cf0
Jonathan Martinez <jon.martinez@colostate.edu>
2026-07-28 15:45:02 -0400

    Set default nudging configuration
    State 1 = 4 days
    State 2 = 3 days
    
    Transition from state 1 -> state 2 after day 4

--- TVPDS/nudge/generate_nudge_files.bash
+++ TVPDS/nudge/generate_nudge_files.bash
@@ -6,20 +6,18 @@
 intervals_per_day=1
 # 3-hour interval (24/3)
 #intervals_per_day=8
-days_state1=5
-days_state2=2
+days_state1=4
+days_state2=3
 state1_file="wrfinput_d01_state1"
 state2_file="wrfinput_d01_state2"
 
 # Generate state 1 links
-#for ((i=1; i<=days_state1*intervals_per_day; i++)); do
 for ((i=1; i<=days_state1*intervals_per_day+1; i++)); do
   ln -s "$state1_file" "wrfinput_d01_t$(printf '%02d' $i)"
 done
 
 # Generate state 2 links
 for ((j=1; j<=days_state2*intervals_per_day; j++)); do
-  #k=$((days_state1*intervals_per_day + j))
   k=$((days_state1*intervals_per_day + j+1))
   ln -s "$state2_file" "wrfinput_d01_t$(printf '%02d' $k)"
 done
